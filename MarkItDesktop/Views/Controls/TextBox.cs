4c8b4d23900a6cb4509310578d82424241fc653b
sudo-bluee <abdelhakim.merzoug00@gmail.com>
2022-09-24 14:41:16 +0100

    Added fonts, default styles, styled login page

---
+++ MarkItDesktop/Views/Controls/TextBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarkItDesktop.Views.Controls
+{
+    public class TextBox : System.Windows.Controls.TextBox
+    {
+
+        public string PlaceHolder
+        {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(TextBox), new PropertyMetadata(string.Empty));
+
+
+    }
+}
